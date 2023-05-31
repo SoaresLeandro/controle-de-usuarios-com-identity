afc9794a801fe78bff7fbbcb2388c5d92ce417e7
Leandro da Silva Soares <3973@ucdb.br>
2023-05-31 13:34:46 -0400

    Início do Projeto, recebendo um Usuário

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+
+namespace UsuariosApi.Controllers
+{
+    [ApiController]
+    [Route("{Controller}")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastrarUsuario([FromBody] CreateUsuarioDto usuarioDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data/Dtos/CreateUsuarioDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsuariosApi.Data.Dtos
+{
+    public class CreateUsuarioDto
+    {
+        [Required]
+        public string NomeDeUsuario { get; set; }
+        [Required]
+        public DateTime DataNascimento { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+        [Required]
+        [Compare("Senha")]
+        public string ConfirmacaoDeSenha { get; set; }
+    }
+}
