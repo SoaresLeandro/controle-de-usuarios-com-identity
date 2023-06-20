1c0d93f228e337a1ac4a1ff50e0f1a964d262cb5
Leandro da Silva Soares <3973@ucdb.br>
2023-06-20 08:47:11 -0400

    Autenticando via Token

---
+++ Authorization/IdadeAuthorization.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace UsuariosApi.Authorization
+{
+    public class IdadeAuthorization : AuthorizationHandler<IdadeMinima>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IdadeMinima requirement)
+        {
+            var dataNascimentoClaim = context.User.FindFirst(claim => claim.Type == ClaimTypes.DateOfBirth);
+
+            if(dataNascimentoClaim is null) return Task.CompletedTask;
+
+            var dataNascimento = Convert.ToDateTime(dataNascimentoClaim.Value);
+
+            if((DateTime.Now.Year - DateTime.Today.AddYears(-dataNascimento.Year).Year) >= requirement.Idade)
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -4,15 +4,41 @@ using UsuariosApi.Data;
 using UsuariosApi.Models;
 using AutoMapper;
 using UsuariosApi.Services;
+using UsuariosApi.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<IAuthorizationHandler, IdadeAuthorization>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ApiIdentityLeandroApiIdentityLeandro")),
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddAuthorization(options => 
+{
+    options.AddPolicy("IdadeMinima", policy => 
+        policy.AddRequirements(new IdadeMinima(18)));
+});
 
 builder.Services.AddScoped<UsuarioService>();
 builder.Services.AddScoped<TokenService>();
@@ -35,11 +61,17 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if(app.Environment.IsDevelopment())
 {
+    //app.UseAuthentication();
+
+    //app.UseAuthorization();
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
